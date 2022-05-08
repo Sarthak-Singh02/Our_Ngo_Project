8830432d2737581eabfb51d0616aca585a432ed0
Ayush Kumar <84243243+ayushk0@users.noreply.github.com>
2022-05-08 17:13:08 +0530

    Update MyRoutes.dart

--- lib/utilities/MyRoutes.dart
+++ lib/utilities/MyRoutes.dart
@@ -1,5 +1,6 @@
 class MyRoutes {
   static String homeRoute = '/home';
+  static String loginRoute = '/login';
   static String donateRoute = '/donate';
   static String submitRoute = '/submit';
   static String helpUsRoute = '/helpUs';
