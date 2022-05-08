9b466cde9b60720d18eb559d9f51404d7ecddee2
Ayush Kumar <84243243+ayushk0@users.noreply.github.com>
2022-05-08 17:24:14 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class _MyAppState extends State<MyApp> {
       initialRoute: MyRoutes.loginRoute,
       routes: {
         MyRoutes.homeRoute: (context) => const HomePage(),
+        MyRoutes.homeRoute: (context) => const LoginPage(),
         MyRoutes.donateRoute: (context) => DonatePage(),
         MyRoutes.submitRoute: (context) => Submitted(),
         MyRoutes.helpUsRoute: (context) => HelpUs(),
